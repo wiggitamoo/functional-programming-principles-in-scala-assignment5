1fae13d37650d1081906c28fa9ce43db378167a8
Andre Henriques <andre.henriques@optimalstrategix.com>
2017-03-27 09:52:16 -0400

    Adds combinations

--- src/main/scala/forcomp/Anagrams.scala
+++ src/main/scala/forcomp/Anagrams.scala
@@ -85,7 +85,15 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = occurrences match {
+    case List() => List[Occurrences](List())
+    case (char, numChars) :: occurrences => {
+      for { result <- combinations(occurrences); i <- 0 to numChars} yield {
+        if (i == 0) result sortBy (_._1)
+        else (result :+ ((char, i))) sortBy (_._1)
+      }
+    }
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
